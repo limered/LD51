ce353db3a59a4a8a74f51097bf563dc3f34c4863
limered <me@limered.de>
2022-09-30 23:10:49 +0200

    add simple test system

--- game/Assets/Systems/Test/TestComponent.cs
+++ game/Assets/Systems/Test/TestComponent.cs
@@ -1,8 +1,13 @@
 ﻿using SystemBase.Core;
+using UnityEngine;
 
 namespace Systems.Test
 {
     public class TestComponent : GameComponent
     {
+        [SerializeField] public Vector3 speed;
+        [SerializeField] public Bounds bounds;
+
+        [HideInInspector] public Vector3 direction;
     }
 }
--- game/Assets/Systems/Test/TestSystem.cs
+++ game/Assets/Systems/Test/TestSystem.cs
@@ -1,6 +1,8 @@
-using System;
 using SystemBase.Core;
+using UniRx;
+using Unity.Mathematics;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Systems.Test
 {
@@ -9,7 +11,19 @@ namespace Systems.Test
     {
         public override void Register(TestComponent component)
         {
-            Debug.Log("Component Registered " + component.name);
+            SystemUpdate(component).Subscribe(Animate).AddTo(component);
+        }
+
+        private void Animate(TestComponent test)
+        {
+            var nextPosition = test.transform.position +
+                               (Vector3) (math.float3(test.direction) *
+                                          math.float3(test.speed));
+
+            if (!test.bounds.Contains(nextPosition))
+                test.direction = Vector3.Normalize(test.bounds.center - nextPosition + Random.insideUnitSphere);
+
+            test.transform.position = nextPosition;
         }
     }
 }
