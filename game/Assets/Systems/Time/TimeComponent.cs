55b101e1dcb267e3cfadc2348436abc33c083901
limered <me@limered.de>
2022-10-01 00:45:26 +0200

    add a timer

---
+++ game/Assets/Systems/Time/TimeComponent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SystemBase.Core;
+using UniRx;
+
+namespace Systems.Time
+{
+    public class TimeComponent : GameComponent
+    {
+        public float tickDurationInSeconds;
+        
+        public float startTime;
+        public FloatReactiveProperty progress = new(1);
+        
+        [NonSerialized] public readonly ReactiveCommand tick = new();
+    }
+}
