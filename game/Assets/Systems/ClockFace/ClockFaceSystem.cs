894713f63c1a4a8f01fec735a349bb3d81daa811
limered <me@limered.de>
2022-10-01 17:50:05 +0200

    watch time pass

--- game/Assets/Systems/ClockFace/ClockFaceSystem.cs
+++ game/Assets/Systems/ClockFace/ClockFaceSystem.cs
@@ -10,7 +10,7 @@ namespace Systems.ClockFace
     public class ClockFaceSystem : GameSystem<ClockFaceComponent, TimeComponent>
     {
         private readonly ReactiveProperty<TimeComponent> _timer = new(null);
-
+        
         public override void Register(ClockFaceComponent clock)
         {
             _timer.WhereNotNull().Subscribe(_ => StartTicking(clock)).AddTo(clock);
@@ -23,18 +23,16 @@ namespace Systems.ClockFace
 
         private void AnimateHands(ClockFaceComponent clock)
         {
-            
+            var progress = _timer.Value.progress.Value;
+
+            var handTransform = clock.SecondHand.transform;
+            // clock.SecondHand.transform.localRotation = Quaternion.identity;
+            handTransform.rotation = Quaternion.Euler(0, 0, progress * -360);
         }
 
         public override void Register(TimeComponent component)
         {
             _timer.Value = component;
         }
-    }
-
-    public class ClockFaceComponent : GameComponent
-    {
-        public GameObject MainHand;
-        public GameObject SecondHand;
     }
 }
