c2aaa17b2ff8fb6af0a9e9313a07a930c1fd17aa
limered <me@limered.de>
2022-10-01 17:19:54 +0200

    subscribe clock face to timer

--- game/Assets/Systems/ClockFace/ClockFaceSystem.cs
+++ game/Assets/Systems/ClockFace/ClockFaceSystem.cs
@@ -1,13 +1,34 @@
 ﻿using SystemBase.Core;
+using SystemBase.Utils;
+using Systems.Time;
+using UniRx;
 using UnityEngine;
 
 namespace Systems.ClockFace
 {
-    public class ClockFaceSystem : GameSystem<ClockFaceComponent>
+    [GameSystem]
+    public class ClockFaceSystem : GameSystem<ClockFaceComponent, TimeComponent>
     {
-        public override void Register(ClockFaceComponent component)
+        private readonly ReactiveProperty<TimeComponent> _timer = new(null);
+
+        public override void Register(ClockFaceComponent clock)
+        {
+            _timer.WhereNotNull().Subscribe(_ => StartTicking(clock)).AddTo(clock);
+        }
+
+        private void StartTicking(ClockFaceComponent clock)
+        {
+            SystemUpdate(clock).Subscribe(AnimateHands).AddTo(clock);
+        }
+
+        private void AnimateHands(ClockFaceComponent clock)
         {
             
+        }
+
+        public override void Register(TimeComponent component)
+        {
+            _timer.Value = component;
         }
     }
 
