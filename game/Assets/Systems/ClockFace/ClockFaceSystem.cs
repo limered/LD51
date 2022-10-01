b52cdca1a0f1b6fb11139ae63116bf7bcef34896
limered <me@limered.de>
2022-10-01 15:18:17 +0200

    add clock face system

---
+++ game/Assets/Systems/ClockFace/ClockFaceSystem.cs
@@ -0,0 +1,19 @@
+﻿using SystemBase.Core;
+using UnityEngine;
+
+namespace Systems.ClockFace
+{
+    public class ClockFaceSystem : GameSystem<ClockFaceComponent>
+    {
+        public override void Register(ClockFaceComponent component)
+        {
+            
+        }
+    }
+
+    public class ClockFaceComponent : GameComponent
+    {
+        public GameObject MainHand;
+        public GameObject SecondHand;
+    }
+}
