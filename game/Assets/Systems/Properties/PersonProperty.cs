ce057457bdc96c66acaed302a34931c595d284f0
limered <me@limered.de>
2022-10-01 16:10:52 +0200

    add person property SO

---
+++ game/Assets/Systems/Properties/PersonProperty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using UnityEngine;
+
+namespace Systems.Properties
+{
+    public enum PropertyLocation
+    {
+        Image,
+        Text
+    }
+    
+    [CreateAssetMenu]
+    public class PersonProperty  : ScriptableObject
+    {
+        public Guid guid = new();
+        public string name;
+        public string text;
+        public PropertyLocation propertyLocation;
+    }
+}
