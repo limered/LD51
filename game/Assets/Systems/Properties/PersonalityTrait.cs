56bbc4e57c2101e242ce5c4acad2fecad2532648
Michael Scherbakow <ryupold@gmail.com>
2022-10-01 17:14:50 +0200

    refactor personality traits

---
+++ game/Assets/Systems/Properties/PersonalityTrait.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+
+namespace Systems.Properties
+{
+    public enum PropertyLocation
+    {
+        Image,
+        Text,
+    }
+
+    public enum Category
+    {
+        Name,
+        FoodType,
+        Pets,
+        Sport,
+    }
+    
+    [CreateAssetMenu]
+    public class PersonalityTrait  : ScriptableObject
+    {
+        public Guid guid = new();
+        public Category category;
+        public string text;
+        public PropertyLocation propertyLocation;
+    }
+}
---
+++ game/Assets/Systems/Properties/ProfileText.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Systems.Properties
+{   
+    /// <summary>
+    /// template text contains placeholder in form {??}
+    /// {name}: Anna Bell, Irvin Irwich, ...
+    /// {food_type}: vegetarian, carnivore, vegan, paleo 
+    /// {hobby}: tennis, football, cooking, tattoos, netflix, swimming
+    /// {travel_location}: paris, north pole, hell, disneyland, ...
+    /// {random}: vegetarian, carnivore, vegan, paleo, ...
+    /// {religion}: dooms day cult, happy tree friends, atheism, ...
+    /// {degree}: hard, soft, long, cool, hot, dark, bright,...
+    /// {relationship}: friends, homies, bros, family, sisters, coworkers, ... 
+    /// </summary>
+    [CreateAssetMenu]
+    public class ProfileText : ScriptableObject
+    {
+        public Guid guid = new();
+        public string template;
+
+        public IEnumerable<PersonalityTrait> Traits
+        {
+            get
+            {
+                return null;
+            }
+        }
+        
+        public string Create(IEnumerable<PersonalityTrait> traits)
+        {
+            return null;
+        }
+    }
+}
