220389e649576bdce7d9766b7dd6dcdd40aca28c
Sven Milker <s.m.milker@gmail.com>
2022-10-01 18:02:57 +0200

    add profile displaying

---
+++ game/Assets/Systems/Profile/Profile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Systems.Profile
+{
+    [CreateAssetMenu]
+    public class Profile : ScriptableObject
+    {
+        public Sprite avatar;
+        public new string name;
+    }
+}
---
+++ game/Assets/Systems/Profile/ProfileComponent.cs
@@ -0,0 +1,12 @@
+﻿using SystemBase.Core;
+using TMPro;
+using UnityEngine.UI;
+
+namespace Assets.Systems.Profile
+{
+    public class ProfileComponent : GameComponent
+    {
+        public Image image;
+        public new TextMeshProUGUI name;
+    }
+}
