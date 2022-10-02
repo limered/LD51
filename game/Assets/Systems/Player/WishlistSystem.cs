f47207f2281a3a53b5ce187aac564bf7d0303a7d
limered <me@limered.de>
2022-10-02 15:40:24 +0200

    wishlist gets filled with traits

---
+++ game/Assets/Systems/Player/WishlistSystem.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Assets.Utils;
+using SystemBase.Core;
+using SystemBase.Utils;
+using Systems.Profile;
+using Systems.Profile.Events;
+using UniRx;
+using UnityEngine;
+
+namespace Systems.Player
+{
+    [GameSystem]
+    public class WishlistSystem : GameSystem<WishlistComponent>
+    {
+        public override void Register(WishlistComponent wishList)
+        {
+            FillWishes(wishList);
+
+            wishList.likedProfiles = new List<DisplayProfile>();
+
+            MessageBroker.Default.Receive<ActiveProfileChangedEvent>()
+                .Subscribe(msg => SaveLikedProfiles(msg, wishList))
+                .AddTo(wishList);
+        }
+
+        private void FillWishes(WishlistComponent wishList)
+        {
+            var allTraits = ScriptableObjectSearcher.GetAllPersonalityTraits().Randomize();
+            var positivesCount = Random.Range(wishList.wantPositivesMinMax.x, wishList.wantPositivesMinMax.y);
+            var negativesCount = Random.Range(wishList.wantNegativeMinMax.x, wishList.wantNegativeMinMax.y);
+
+            wishList.wantPositives = allTraits.GetRange(0, positivesCount)
+                .Select(trait => new CheckedTrait {trait = trait, state = PersonalityCheckState.Unchecked})
+                .ToList();
+            wishList.wantNegatives = allTraits.GetRange(positivesCount + 1, positivesCount + 1 + negativesCount)
+                .Select(trait => new CheckedTrait {trait = trait, state = PersonalityCheckState.Unchecked})
+                .ToList();
+        }
+
+        private void SaveLikedProfiles(ActiveProfileChangedEvent msg, WishlistComponent wishList)
+        {
+            if (msg.lastProfile.Rating == Rating.Dislike) return;
+            wishList.likedProfiles.Add(msg.lastProfile);
+
+            var profileTraits = msg.lastProfile.AllTraits();
+            var foundPositive = wishList.wantPositives.FindAll(trait => profileTraits.Contains(trait.trait));
+            var foundNegatives = wishList.wantNegatives.FindAll(trait => profileTraits.Contains(trait.trait));
+            foreach (var checkedTrait in foundPositive)
+            {
+                checkedTrait.state = PersonalityCheckState.Checked;
+            }
+            // Check for Win
+
+            foreach (var checkedTrait in foundNegatives)
+            {
+                checkedTrait.state = PersonalityCheckState.Checked;
+            }
+            // Check for Loss
+        }
+    }
+}
--- game/Assets/Systems/Profile/DisplayProfile.cs
+++ game/Assets/Systems/Profile/DisplayProfile.cs
@@ -1,4 +1,6 @@
-﻿using Systems.Profile.ScriptableObjects;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Systems.Profile.ScriptableObjects;
 
 namespace Systems.Profile
 {
@@ -6,5 +8,12 @@ namespace Systems.Profile
     {
         public ProfileSo Profile { get; set; }
         public Rating? Rating { get; set; }
+
+        public IEnumerable<PersonalityTrait> AllTraits()
+        {
+            var imageTraits = Profile.profileImage.traits;
+            var textTraits = Profile.traits;
+            return imageTraits.Concat(textTraits);
+        }
     }
 }
