e70425b2662c2fa5366d2442ed57a9264a1d75fe
limered <me@limered.de>
2022-10-02 15:40:05 +0200

    add array randomize extension

--- game/Assets/SystemBase/Utils/DotNet/LinqExtensions.cs
+++ game/Assets/SystemBase/Utils/DotNet/LinqExtensions.cs
@@ -12,7 +12,19 @@ namespace SystemBase.Utils
         {
             return coll.OrderBy(x => x).Skip(n - 1).FirstOrDefault();
         }
-        
+
+        public static List<T> Randomize<T>(this T[] list)
+        {
+            var result = new List<T>(list.Length);
+            foreach (var item in list)
+            {
+                var rnd = (int) (Random.value * result.Count);
+                result.Insert(rnd, item);
+            }
+
+            return result;
+        }
+
         public static List<T> Randomize<T>(this List<T> list)
         {
             var result = new List<T>(list.Count);
@@ -22,34 +34,26 @@ namespace SystemBase.Utils
                 result.Add(list[rnd]);
                 list.RemoveAt(rnd);
             }
+
             return result;
         }
-        
+
         public static IEnumerable<T> AddDefaultCount<T>(this IEnumerable<T> coll, int n) where T : new()
         {
             var result = new List<T>();
-            for (var i = 0; i < n; i++)
-            {
-                result.Add(new T());
-            }
+            for (var i = 0; i < n; i++) result.Add(new T());
 
             return coll.Concat(result);
         }
 
         public static void Print<T>(this IEnumerable<T> list)
         {
-            foreach (var element in list)
-            {
-                Debug.Log(element);
-            }
+            foreach (var element in list) Debug.Log(element);
         }
-        
+
         public static void Print<T>(this IEnumerable<T> list, Func<T, string> returnStringToPrint)
         {
-            foreach (var element in list)
-            {
-                Debug.Log(returnStringToPrint(element));
-            }
+            foreach (var element in list) Debug.Log(returnStringToPrint(element));
         }
     }
 }
