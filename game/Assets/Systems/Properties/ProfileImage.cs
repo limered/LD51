1cf3d6a489b585a61b64f01aa52121e4b18d839c
limered <me@limered.de>
2022-10-02 11:38:56 +0200

    add profile image SO

---
+++ game/Assets/Systems/Properties/ProfileImage.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Systems.Properties
+{
+    [CreateAssetMenu(menuName = "LD51/ProfileImage", fileName = "ProfileImage")]
+    public class ProfileImage : ScriptableObject
+    {
+        public Sprite avatar;
+        public PersonalityTrait[] traits;
+    }
+}
