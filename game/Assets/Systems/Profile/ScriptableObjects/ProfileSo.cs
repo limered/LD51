e70cd37bf80ae967269431c28fa4ac57e55afb93
limered <me@limered.de>
2022-10-02 12:09:34 +0200

    add image so to profile

--- game/Assets/Systems/Profile/ScriptableObjects/ProfileSo.cs
+++ game/Assets/Systems/Profile/ScriptableObjects/ProfileSo.cs
@@ -5,6 +5,7 @@ namespace Systems.Profile.ScriptableObjects
     [CreateAssetMenu(menuName = "LD51/Profile", fileName = "Profile", order = 0)]
     public class ProfileSo : ScriptableObject
     {
+        public ProfileImage profileImage;
         public Sprite avatar;
         public new string name;
         public string text;
