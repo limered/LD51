d327943fb7b8017c2c536968408de660b61da3af
Michael Scherbakow <ryupold@gmail.com>
2022-10-03 02:44:41 +0200

    show particles on start

--- game/Assets/Systems/Menu/StartButtonComponent.cs
+++ game/Assets/Systems/Menu/StartButtonComponent.cs
@@ -8,10 +8,17 @@ namespace Systems.Menu
     public class StartButtonComponent : GameComponent
     {
         public GameObject hideOnStart;
+        public GameObject particles;
         public void StartGame()
         {
             MessageBroker.Default.Publish(new GameMsgStart());
             hideOnStart.SetActive(false);
+
+            if (particles)
+            {
+                var clone = GameObject.Instantiate(particles, hideOnStart.transform.parent, true);
+                clone.GetComponent<ParticleSystem>().Emit(100);
+            }
         }
     }
 }
