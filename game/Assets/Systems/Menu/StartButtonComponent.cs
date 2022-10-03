83fdbee7b5c2c379ce7a21793a0203a5afdce254
Michael Scherbakow <ryupold@gmail.com>
2022-10-03 02:09:59 +0200

    add start screen

---
+++ game/Assets/Systems/Menu/StartButtonComponent.cs
@@ -0,0 +1,17 @@
+﻿using SystemBase.Core;
+using SystemBase.GameState.Messages;
+using UniRx;
+using UnityEngine;
+
+namespace Systems.Menu
+{
+    public class StartButtonComponent : GameComponent
+    {
+        public GameObject hideOnStart;
+        public void StartGame()
+        {
+            MessageBroker.Default.Publish(new GameMsgStart());
+            hideOnStart.SetActive(false);
+        }
+    }
+}
