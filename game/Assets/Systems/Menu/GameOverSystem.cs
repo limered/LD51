841e1f525284878532eae15ebdbbd6291e3a471a
Michael Scherbakow <ryupold@gmail.com>
2022-10-03 21:18:37 +0200

    add loose screen

--- game/Assets/Systems/Menu/GameOverSystem.cs
+++ game/Assets/Systems/Menu/GameOverSystem.cs
@@ -13,12 +13,22 @@ namespace Systems.Menu
             MessageBroker.Default.Receive<WinMessage>()
                 .Subscribe((msg) =>
                 {
+                    component.loseContainer.SetActive(false);
+                    component.resultContainer.SetActive(true);
                     foreach (var profile in msg.profiles)
                     {
                         var result = GameComponent.Instantiate(component.loveResultPrefab, component.resultContainer.transform);
                         result.GetComponent<LoveResultComponent>().image.sprite = profile.Profile.profileImage.avatar;
                         result.GetComponent<LoveResultComponent>().text.text = profile.Profile.traits.Aggregate("", (p, n) => p + ", " + n.text );
                     }
+                })
+                .AddTo(component);
+            
+            MessageBroker.Default.Receive<LoseMessage>()
+                .Subscribe((msg) =>
+                {
+                    component.loseContainer.SetActive(true);
+                    component.resultContainer.SetActive(false);
                 })
                 .AddTo(component);
         }
